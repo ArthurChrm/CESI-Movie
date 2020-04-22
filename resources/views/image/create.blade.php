0428ceaab1f77a26d729308bc4ba811f7272459b
Arthur Chéramy <arthur.cheramy@gmail.com>
2020-04-22 16:03:52 +0200

    Affichage du nombre de secondes quand le slider se met à jour

--- resources/views/image/create.blade.php
+++ resources/views/image/create.blade.php
@@ -8,14 +8,17 @@
         <label class="custom-file-label" for="image">Image</label>
     </div>
     <div class="form-group">
-        <label for="duree_image">Durée d'apparition (en secondes):</label>
-        <input class="custom-range" type="range" id="duree_image" name="duree_image" min="0" max="10" step="0.5">
+        <label for="duree_image">Durée d'apparition (en secondes):
+            <output name="dureeOutput" id="dureeOutputId">5</output>
+        </label>
+        <input class="custom-range" type="range" id="duree_image" name="duree_image" min="0" max="10" step="0.5" oninput="dureeOutputId.value = duree_image.value">
+
         <small id="emailHelp" class="form-text text-muted">Cette valeur pourra être modifiée plus tard.</small>
     </div>
     <div class="form-group">
-    <input type='hidden' name='id_projet' value={{$id_projet}} />
+        <input type='hidden' name='id_projet' value={{$id_projet}} />
     </div>
-    
+
     <button type="submit" class="btn btn-primary">Ajouter</button>
 </form>
 @endsection
