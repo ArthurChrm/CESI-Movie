e587c54aa12c29f7e128af6824bce16f09b35a7d
Corgniard <corgniard.antoine@gmail.com>
2020-04-22 16:27:17 +0200

    Div form for image and project

--- resources/views/image/create.blade.php
+++ resources/views/image/create.blade.php
@@ -1,13 +1,15 @@
 @extends('layout')
 
 @section('body')
+<div style="display: flex; justify-content: space-around; margin-top: 5%">
+<div class="p-5 border border-primary rounded bg-light">
 <form method="POST" action="/image/create" enctype="multipart/form-data">
     @csrf
     <div class="custom-file">
         <input class="custom-file-input" id="image" name="image" type='file'>
         <label class="custom-file-label" for="image">Image</label>
     </div>
-    <div class="form-group">
+    <div class="form-group pt-5">
         <label for="duree_image">Durée d'apparition (en secondes):</label>
         <input class="custom-range" type="range" id="duree_image" name="duree_image" min="0" max="10" step="0.5">
         <small id="emailHelp" class="form-text text-muted">Cette valeur pourra être modifiée plus tard.</small>
@@ -18,4 +20,6 @@
     
     <button type="submit" class="btn btn-primary">Ajouter</button>
 </form>
+</div>
+</div>
 @endsection
--- resources/views/projet/create.blade.php
+++ resources/views/projet/create.blade.php
@@ -1,17 +1,23 @@
 @extends('layout')
 
 @section('body')
+<div style="display: flex; justify-content: space-around; margin-top: 5%">
+<div class="p-5 border border-primary rounded bg-light" >
 <form method="POST" action="/projet/create" enctype="multipart/form-data">
 @csrf
     <div class="form-group">
-        <label for="nom_projet">Nom du projet</label>
+        <label for="nom_projet"><h1>Nom du projet</h1></label>
         <input class="form-control" id="nom_projet" name="nom_projet" aria-describedby="emailHelp" placeholder="Entrez le nom du projet">
         <small id="emailHelp" class="form-text text-muted">Le nom vous servira à identifier votre projet.</small>
     </div>
     <div class="form-group">
-        <label for="description">Description du projet</label>
+        <label for="description"><h2>Description du projet</h2></label>
         <textarea class="form-control" id="description" name="description" rows="3" placeholder="Entrez la description du projet"></textarea>
     </div>
-    <button type="submit" class="btn btn-primary">Créer</button>
+    <div style="display: flex; justify-content: center">
+        <button type="submit" class="btn btn-primary border-dark" style="width: 50%">Créer</button>
+    </div>
 </form>
+</div>
+</div>
 @endsection
