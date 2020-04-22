9f2cf564038c5fe1375c96a5e832a66b80cca88b
Arthur Chéramy <arthur.cheramy@gmail.com>
2020-04-22 11:04:46 +0200

    Ajout données brutes sur page home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,22 @@
 
 @section('body')
 <h1>Bonjour !</h1>
+
+
+<div class="container">
+    <div class="row">
+        @for($i = 0; $i <= 10; $i++) <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src={{ URL::to('/images/placeholder.png') }} alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Projet test</h5>
+                    <p class="card-text">Descrition du projet.</p>
+                    <a href="/modifier" class="btn btn-primary">Go !</a>
+                </div>
+            </div>
+    </div>
+    @endfor
+</div>
+</div>
+
 @endsection
---
+++ resources/views/modifier.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('body')
+<div id="fenetre_d_affichage">
+</div>
+
+<div class="row">
+
+
+
+</div>
+@endsection
